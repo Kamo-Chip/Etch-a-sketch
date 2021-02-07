9be4c80b12de33b8a4996998c88bd03affc306d6
Kamogelo Khumalo <kamokhumalo04@gmail.com>
2021-02-07 17:42:54 +0200

    Added erase, rainbow and other methods

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ const container = document.querySelector("#container");
 const body = document.querySelector("body");
 let squares;
 
-const clear = document.createElement("button");
-let htmlStyles = window.getComputedStyle(document.querySelector("html"));
-let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
-let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
+const clear = document.querySelector("button");
+const colorPick = document.querySelector("#color");
+const rainbow = document.querySelector("#rainbow");
+const erase = document.querySelector("#erase");
 const slider = document.getElementsByClassName("slider")[0];
 let grid = 0;
 
 slider.oninput = function(){
 grid = slider.value;
+for(let i = 0; i < squares.length; i++){
+    container.removeChild(squares[i]);
+}
 initialise(grid);
 }
-
 
 function initialise(gridNumber){
     let gridArea = gridNumber * gridNumber;
@@ -21,20 +23,36 @@ function initialise(gridNumber){
     for(let i = 0; i < gridArea; i++){
         squares[i] = document.createElement("div");
         squares[i].classList.add("block");
-        container.style.gridTemplateColumns = `repeat(${gridNumber},80px)`;
-        container.style.gridTemplateRows = `repeat(${gridNumber}, 80px)`;
+        container.style.gridTemplateColumns = `repeat(${gridNumber},auto)`;
+        container.style.gridTemplateRows = `repeat(${gridNumber}, auto)`;
         container.appendChild(squares[i]);
     }
-
-    clear.classList.add("clear-btn");
-    clear.textContent = "Clear";
-    body.appendChild(clear);
 } 
 
-function changeColor(e){
-    e.target.style.backgroundColor = "#000";
+function rainbowColor(){
+    let randomColor = "#"+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
 }
 
+function changeColor(e){
+    if(e.target.style.backgroundColor == "" || e.target.style.backgroundColor == "rgb(255, 255, 255)"){
+        if(rainbow.checked == true){
+            e.target.style.backgroundColor = rainbowColor();
+        }else{
+            e.target.style.backgroundColor = colorPick.value;
+        }
+    }
+}
+
+function eraseBlock(e){
+    e.target.style.backgroundColor = "#fff";
+}
+
+window.addEventListener("mouseover", function(e){
+    if((e.target.style.backgroundColor != "rgb(255, 255, 255)" || e.target.style.backgroundColor != "") && e.target.className == "block" && erase.checked == true){
+        eraseBlock(e);
+    }
+})
 function clearColor(){
     for(let i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = "#fff";
